f4674c62fd4d9bf11dfb250935bba8ac05a1f06a
Oisin Mulvihill <oisin.mulvihill@gmail.com>
2020-06-08 16:02:55 +0100

    realised I have to update tests for both code bases is two different places, need to resolve this smell

--- test/test_embedded/test_code.cpp
+++ test/test_embedded/test_code.cpp
@@ -7,10 +7,11 @@ Oisin Mulvihill
 #include <Arduino.h>
 #include <unity.h>
 #include "number.h"
+#include "logic.h"
 
 
 void test_decimalToString(void) {
-    char output[6] = {0};
+    char output[7] = {0};
     
     TEST_ASSERT_EQUAL_STRING(
         "34.16", decimalToString(output, 6, 34.1623)
@@ -25,13 +26,49 @@ void test_decimalToString(void) {
     );
     
     TEST_ASSERT_EQUAL_STRING(
-        "4.1", decimalToString(output, 6, 4.1)
+        "4.10", decimalToString(output, 6, 4.1)
     );
     
     TEST_ASSERT_EQUAL_STRING(
         "122.1", decimalToString(output, 6, 122.1)
     );
     
+    TEST_ASSERT_EQUAL_STRING(
+        "0.00", decimalToString(output, 6, 0)
+    );
+    
+    TEST_ASSERT_EQUAL_STRING(
+        "1.10", decimalToString(output, 6, 1.1)
+    );
+
+    TEST_ASSERT_EQUAL_STRING(
+        "2.20", decimalToString(output, 6, 2.2)
+    );
+    
+    TEST_ASSERT_EQUAL_STRING(
+        "12.10", decimalToString(output, 6, 12.1)
+    );
+
+    TEST_ASSERT_EQUAL_STRING(
+        "-1.22", decimalToString(output, 6, -1.22)
+    );
+
+    TEST_ASSERT_EQUAL_STRING(
+        "-1.10", decimalToString(output, 6, -1.1)
+    );
+
+    TEST_ASSERT_EQUAL_STRING(
+        "-99.10", decimalToString(output, 7, -99.1)
+    );
+
+    TEST_ASSERT_EQUAL_STRING(
+        "-99.11", decimalToString(output, 7, -99.11)
+    );
+
+    TEST_ASSERT_EQUAL_STRING(
+        "-99.12", decimalToString(output, 7, -99.12)
+    );
+
     // The buffer is not big enough so you will see example of it breaking. You
     // need to make sure output is big enough for maximum number of digits + 
     // '.' + \0 character. The fraction part is always two digits.
@@ -50,7 +87,66 @@ void test_decimalToString(void) {
 }
 
 void test_generateReport(void) {
+    // Maximum string size I expect "t=XXX.YY&h=XXX.YY&d=XXX.YY\0" (27)
+    char report[27] = {0};
 
+    generateReport(
+        report, 
+        sizeof(report), 
+        21.322, 
+        34.871,
+        17.12
+    );
+    TEST_ASSERT_EQUAL_STRING("t=21.32&h=34.87&d=17.12", report);
+
+    generateReport(
+        report, 
+        sizeof(report), 
+        99.99, 
+        100.0,
+        0.0
+    );
+    TEST_ASSERT_EQUAL_STRING("t=99.99&h=100.00&d=0.00", report);
+
+    generateReport(
+        report, 
+        sizeof(report), 
+        1.10, 
+        2.20,
+        3.30
+    );
+    TEST_ASSERT_EQUAL_STRING("t=1.10&h=2.20&d=3.30", report);
+}
+
+void test_generateHTTPPOST(void) {
+    char request[MAX_REQUEST_LINES][MAX_REQUEST_LINE_SIZE] = {0};
+
+    generateHTTPPost(request, "tarsis", "t=22.87&h=33.34&d=12.4");
+
+    TEST_ASSERT_EQUAL_STRING(
+        "POST /log/sample/indoor HTTP/1.0", 
+        request[0]
+    );
+    TEST_ASSERT_EQUAL_STRING(
+        "Host: tarsis", 
+        request[1]
+    );
+    TEST_ASSERT_EQUAL_STRING(
+        "Content-length: 22", 
+        request[2]
+    );
+    TEST_ASSERT_EQUAL_STRING(
+        "Content-Type: application/x-www-form-urlencoded", 
+        request[3]
+    );
+    TEST_ASSERT_EQUAL_STRING(
+        "\n", 
+        request[4]
+    );
+    TEST_ASSERT_EQUAL_STRING(
+        "t=22.87&h=33.34&d=12.4", 
+        request[5]
+    );
 }
 
 void setup() {
@@ -60,5 +156,6 @@ void setup() {
 void loop() {
     RUN_TEST(test_decimalToString);
     RUN_TEST(test_generateReport);
+    RUN_TEST(test_generateHTTPPOST);
     UNITY_END();
 }
